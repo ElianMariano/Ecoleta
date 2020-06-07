016fe2f93382cce800ae96ffc86d34b624304465
Elian <elianmarianogabriel@gmail.com>
2020-06-07 10:31:04 -0300

    Dia 4 - Criação App mobile

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image: `http://localhost:3333/uploads/${item.image}`,
+                image: `http://192.168.0.108:3333/uploads/${item.image}`,
             }
         });
 
