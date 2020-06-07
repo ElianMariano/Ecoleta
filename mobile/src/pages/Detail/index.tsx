b7830f64d3557370e97ec02cedbe4448ca784e20
Elian <elianmarianogabriel@gmail.com>
2020-06-07 16:28:49 -0300

    Dia 5 - Upload de imagens e validação

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -14,6 +14,7 @@ interface Params {
 interface Data {
   point: {
     image: string;
+    image_url: string;
     name: string;
     email: string;
     whatsapp: string;
@@ -22,7 +23,6 @@ interface Data {
   };
   items: {
     title: string;
-
   }[];
 }
 
@@ -65,7 +65,7 @@ const Detail = () => {
                   <Icon name="arrow-left" size={20} color='#34CB79' />
               </TouchableOpacity>
 
-              <Image style={styles.pointImage} source={{uri: data.point.image}}/>
+              <Image style={styles.pointImage} source={{uri: data.point.image_url }}/>
 
               <Text style={styles.pointName}>{data.point.name}</Text>
               <Text style={styles.pointItems}>
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,16 +1,39 @@
 import express from 'express';
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
+import multer from 'multer';
+import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 // index, create, show, update, delete
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
-routes.post('/points', pointController.create);
 routes.get('/points/:id', pointController.show);
 routes.get('/points', pointController.index);
+
+routes.post('/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.string().required(),
+            longitude: Joi.string().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required()
+        }),
+    }, {
+        abortEarly: false
+    }),
+    pointController.create
+);
 
 export default routes;
